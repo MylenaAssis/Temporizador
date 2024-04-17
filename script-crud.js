6122f61a27fbd0b4bef383916ece5cf01b3e4b17
Mylena Assis <lena.assis.dev@gmail.com>
2024-04-17 13:52:24 -0300

    Fixed - correcao svg na lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -13,12 +13,10 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg') //criando o elemento svg no html
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p')
@@ -35,7 +33,7 @@ function criarElementoTarefa(tarefa) {
     li.append(paragrafo)
     li.append(botao)
 
-    return li
+    return li //apos inserir tudo no li, retornar o elemento criado
 }
 
 //pega o evento click no botão para chamar o formulário. Por meio do toggle faz o formulario aparecer ou sumir
